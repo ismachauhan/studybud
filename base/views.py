f0f8ebb039739ed1158e6f29b9a31e2d22651bbc
Isma Chauhan <ismachauhan8@gmail.com>
2026-01-06 21:17:36 +0530

    database and admin panel completed

---
+++ base/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render 
+
+from .models import Room
+
+# rooms=[
+#     {'id':1,'name':'lets learn python'},
+#     {'id':2,'name':'design with me'},
+#     {'id':3,'name':'frontend developer'},
+# ]
+
+
+
+def home(request):
+    rooms=Room.objects.all()
+    context={'rooms': rooms}
+    return render(request,'base/home.html',
+    {'rooms':rooms})
+
+
+def room(request, pk):
+    room=Room.objects.get(id=pk)
+
+    context= {'room':room}
+    return render(request,'base/room.html',context)
